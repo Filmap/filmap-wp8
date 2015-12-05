b11f02cfd5ee435ffff01d182249c282cdb06714
Ivanilson Melo <meloivanilson@gmail.com>
2015-12-05 20:10:05 -0300

    Login e registro
    Adicionei a pagina de registro e coloquei a pagina de login para pegar o
    token na nossa api. Tou fazendo a pagina de registro, mas vou tentar dar
    deploy na api antes.

---
+++ Filmap/Filmap/RegisterPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Net.Http;
+
+namespace Filmap
+{
+    public partial class RegisterPage : PhoneApplicationPage
+    {
+        private string address = "http://heavy-goat-5389.vagrantshare.com";
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            /*button.IsEnabled = false;
+            txtConfirmPassword.IsEnabled = false;
+            progressBar.Visibility = Visibility.Visible;
+            registerFormPanel.Visibility = Visibility.Collapsed;*/
+
+            RegisterUser();
+        }
+
+        private async void RegisterUser()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(address);
+
+
+            var credentials = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string, string>("name", txtName.Text),
+                new KeyValuePair<string, string>("email", txtEmail.Text),
+                new KeyValuePair<string, string>("password", txtPassword.Text),
+                new KeyValuePair<string, string>("confirm_password", txtConfirmPassword.Text)
+            });
+
+            var response = await httpClient.PostAsync("/user", credentials);
+
+            var str = response.Content.ReadAsStringAsync().Result;
+
+            //Token t = JsonConvert.DeserializeObject<Token>(str);
+
+            MessageBox.Show(str);
+        }
+    }
+}
